ab0723d140f90c88311c86775bf65f3a0072ce1d
nezvers <agnis16@inbox.lv>
2019-12-04 21:44:48 +0200

    a

--- Trijam - Bounce or Die/Autoload/Event.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-func _unhandled_input(event):
-	if event.is_action_pressed("exit"):
-		get_tree().quit()
-	elif event.is_action_pressed("restart"):
-		get_tree().reload_current_scene()
